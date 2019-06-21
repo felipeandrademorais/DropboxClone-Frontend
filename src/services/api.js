73527e7f4b2383aade18234e4d320723bec67f4a
FELIPE ANDRADE DE MORAIS <felipe.morais@fiesc.com.br>
2019-06-21 18:06:55 -0300

    Alteração baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://googledriveclone.herokuapp.com/', 
+    baseURL: 'https://186.209.243.57:3000', 
 });
 
 export default api;
