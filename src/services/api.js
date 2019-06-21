36b8d4eb9a2b5c4bbbf6c9acfc43a28029dd2b6c
FELIPE ANDRADE DE MORAIS <felipe.morais@fiesc.com.br>
2019-06-21 18:24:54 -0300

    Modificação api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://manualdeti.com.br:3000', 
+    baseURL: 'https://googledriveclone.herokuapp.com', 
 });
 
 export default api;
